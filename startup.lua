d88ec238ea1097b901669758692d5f095aa05596
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-11-28 15:58:43 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -2,9 +2,17 @@ local file = require("libraries.file")
 local graphic = require("libraries.graphic")
 
 local guiLayerOne = "img/gui-layer-one.skgrp"
+local guiLayerTwo = "img/gui-layer-two.skgrp"
 
 local function drawGui()
   file.loadGrpLines(guiLayerOne)
+  file.loadGrpLines(guiLayerTwo)
 end
 
+term.clear()
+
 drawGui()
+
+local function fillGPS()
+  
+end
