14ad3af2ca1362505917671eb291e8e0f212c6c3
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-11-28 14:35:08 -0500

    Create startup.lua

---
+++ startup.lua
@@ -0,0 +1,8 @@
+local file = require("libraries.file")
+local graphic = require("libraries.graphic")
+
+local guiLayerOne = "img/gui-layer-one.skgrp"
+
+local function drawGui()
+  file.loadGrpLines(guiLayerOne)
+end
