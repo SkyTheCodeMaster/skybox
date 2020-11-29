0a4a0e36445149233d32ccd84d0d760eb91a6e1e
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-11-28 20:38:02 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -16,7 +16,7 @@ drawGui()
 
 local function fillGPS()
   local x,y,z = tm.getPosition()
-  local fX,fY,fZ = math.floor(fX+0.5),math.floor(fY+0.5),math.floor(fZ+0.5)
+  local fX,fY,fZ = math.floor(X+0.5),math.floor(Y+0.5),math.floor(Z+0.5)
   term.setCursorPos(20,5)
   term.write(tostring(fX))
   term.setCursorPos(20,6)
