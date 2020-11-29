a528579a6bb806e8cf68effcb0968c3682097c72
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-11-29 14:55:09 -0500

    Update tm.lua

--- libraries/tm.lua
+++ libraries/tm.lua
@@ -120,7 +120,7 @@ function tm.face(sDirection)
     elseif facing == "west" then tm.turnRight() tm.turnRight()
     elseif facing == "south" then tm.turnLeft() end
   end
-  tm.savePos()
+  tm.save()
 end
 
 function tm.getPosition()
